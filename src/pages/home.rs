fd7f5e51f03a8877d2ff0275fc19ef65962dea45
Xelarse <alexallmandev@gmail.com>
2022-02-15 00:30:20 +0000

    Started adding profile pic to home

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -19,17 +19,26 @@ impl Component for Home {
 
     fn view(&self) -> Html {
         html! {
-            <div class=classes!("tile", "is-ancestor", "is-vertical")>
+            <div class=classes!("columns")>
+                <div class=classes!("column", "is-two-thirds") >
+                    <div class=classes!("tile", "is-ancestor", "is-vertical")>
+                        <div class=classes!("tile", "is-child", "hero")>
+                            <div class=classes!("hero-body", "container", "pb-0")>
+                                <h1 class=classes!("title", "is-1")>{"Welcome to Xel's home"}</h1>
+                            </div>
+                        </div>
 
-                <div class=classes!("tile", "is-child", "hero")>
-                    <div class=classes!("hero-body", "container", "pb-0")>
-                        <h1 class=classes!("title", "is-1")>{"Welcome to Xel's home"}</h1>
+                        <div class=classes!("tile", "is-child")>
+                            <h1 class=classes!("hero-body", "pb-5")>{"Here's links to some socials:"}</h1>
+                            {self.view_socials()}
+                        </div>
                     </div>
                 </div>
 
-                <div class=classes!("tile", "is-child")>
-                    <h1 class=classes!("hero-body", "pb-5")>{"Here's links to some socials:"}</h1>
-                    {self.view_socials()}
+                <div class=classes!("column")>
+                    <figure class=classes!("image", "is-square")>
+                        <img class=classes!("profile-picture") src="/assets/img/me.jpg" alt="Myself and Coheed the Samoyed."/>
+                    </figure>
                 </div>
             </div>
         }
